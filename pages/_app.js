43ed287712148ecd61dac0b63516c696d3777c8e
osomhe1 <osomhe.aleogho@gmail.com>
2024-02-19 21:32:10 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -21,15 +21,15 @@ Router.events.on('routeChangeError', () => {
 })
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
+  // static async getInitialProps({ Component, router, ctx }) {
+  //   let pageProps = {}
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
+  //   if (Component.getInitialProps) {
+  //     pageProps = await Component.getInitialProps(ctx)
+  //   }
 
-    return { pageProps }
-  }
+  //   return { pageProps }
+  // }
   render() {
     const { Component, pageProps } = this.props
 
